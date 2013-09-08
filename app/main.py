bf7075e44855e1067b5a4648af15869aa78700bf
Francis Carr <fran@fcarr.com>
2013-09-08 23:40:40 +0100

    Add domains, remove state, general flow

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask import render_template, make_response, request
+from flask import render_template, make_response, request, redirect
 app = Flask(__name__)
 
 USER_AGENTS = dict()
-STATE = dict()
+
+DOMAIN_ONE = 'www.test.com'
+DOMAIN_TWO = 'http://www.xyz.com'
 
 @app.route("/")
 def index():
@@ -12,12 +14,17 @@ def index():
     query_params = "Query Params: {0}".format(str(request.args.items()))
     response_string = "{0}<br/>{1}<br/>{2}".format(user_agents, cookies, query_params)
 
-    STATE[request.headers.get('User-Agent')] = True
+    user_agent = request.headers.get('User-Agent')
+    host = request.headers.get('Host')
 
-    response = make_response(response_string)
-    response.set_cookie("test", "string")
+    if user_agent not in USER_AGENTS.keys() and host == DOMAIN_ONE:
+        USER_AGENTS[user_agent] = True
+        return redirect(DOMAIN_TWO)
+    else:        
+        response = make_response(response_string)
+        response.set_cookie("test", "string")
 
-    return response
+        return response
 
 if __name__ == "__main__":
     app.debug = True
